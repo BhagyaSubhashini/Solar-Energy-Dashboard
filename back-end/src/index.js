952292fb9244280becd4f145d17d45372cb08c67
Bhagya Wijenayaka <bhagyasubhashini139@gmail.com>
2025-09-29 10:15:16 +0530

    implemented base relations

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -1,12 +1,15 @@
 import "dotenv/config";
 import express from "express";
 import solarUnitRouter from "./api/solar-unit.js";
+import energyGenerationRecordRouter from "./api/energy-generation-record.js";
+
 import { connectDB } from "./infrastructure/db.js";
 
 const server = express();
 server.use(express.json());
 
 server.use("/api/solar-units", solarUnitRouter);
+server.use("/api/energy-generation-records", energyGenerationRecordRouter);
 
 connectDB();
 
